535c6806a856b4f97a67f4fcbed9322ebf60b3b9
teru12012000 <sion0012@outlook.jp>
2022-04-30 15:48:07 +0900

    May

--- wp/src/oda.js
+++ wp/src/oda.js
@@ -4,37 +4,37 @@ export let odaDeta=()=>{
 
   let oda_deta={
     correct:[
+              [y,y,y],
               [n,n,n],
-              [n,n,n],
-              [n,n,n],
-              [n,n,n],
+              [n,n,y],
+              [y,y,y],
               [n,n,n],//5
-              [n,n,n],
-              [n,n,n],
-              [n,n,n],
-              [n,n,n],
-              [n,n,n],//10
-              [n,n,n],
-              [n,n,n],
-              [n,n,n],
-              [n,n,n],
-              [n,n,n],//15
-              [n,n,n],
-              [n,n,n],
-              [n,n,n],
-              [n,n,y],
-              [y,y,y],//20
-              [n,n,n],
               [n,n,y],
               [n,n,y],
               [n,n,n],
-              [n,n,n],//25
-              [n,n,y],
+              [n,n,n],
+              [n,n,y],//10
               [y,y,y],
               [n,n,n],
               [n,n,y],
               [n,n,y],
+              [n,n,n],//15
+              [n,n,n],
               [n,n,y],
+              [y,y,y],
+              [n,n,y],
+              [n,n,y],//20
+              [y,y,y],
+              [n,n,y],
+              [n,n,y],
+              [n,n,y],
+              [n,n,n],//25
+              [n,n,n],
+              [n,n,n],
+              [n,n,n],
+              [n,n,n],
+              [n,n,n],
+              [n,n,n],
             ],
     AM:'amoda',
     PM:'pmoda',
--- wp/src/sinkiba.js
+++ wp/src/sinkiba.js
@@ -1,39 +1,38 @@
 export let sinkibaDeta=()=>{
   const y='〇';
   const n='-';
-  const s='☆';
   let sinkiba_deta={
     correct:[
               [y,y,y],
+              [y,y,y],
+              [n,n,y],
+              [n,n,n],
+              [n,n,n],//5
+              [y,y,y],
               [n,n,n],
               [n,n,n],
-              [y,y,y],
-              [y,y,y],//5
+              [n,n,n],
+              [y,y,y],//10
               [y,y,y],
               [y,y,y],
               [y,y,y],
               [n,n,n],
-              [n,n,n],//10
+              [n,n,n],//15
               [n,n,n],
-              [y,y,y],
-              [y,y,y],
-              [n,n,y],
-              [y,y,y],//15
-              [n,n,y],
-              [n,n,y],
-              [y,y,y],
-              [n,n,y],
-              [y,y,y],//20
-              [y,y,y],
-              [y,y,y],
-              [y,y,y],
-              [n,n,y],
-              [n,n,y],//25
-              [y,y,y],
-              [y,y,y],
-              [y,y,y],
-              [n,y,y],
-              [y,y,y],
+              [n,n,n],
+              [n,n,n],
+              [n,n,n],
+              [n,n,n],//20
+              [n,n,n],
+              [n,n,n],
+              [n,n,n],
+              [n,n,n],
+              [n,n,n],//25
+              [n,n,n],
+              [n,n,n],
+              [n,n,n],
+              [n,n,n],
+              [n,n,n],
               [n,n,n],
             ],
     AM:'amsinkiba',
